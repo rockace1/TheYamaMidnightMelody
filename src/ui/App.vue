dda22e8d7c9cdf0587dd0b44cb20ceb0b41aee4e
shiqiang <shiqiang@jd.com>
2019-12-12 20:59:13 +0800

    update router

--- src/ui/App.vue
+++ src/ui/App.vue
@@ -2,19 +2,18 @@
   <div id="app">
     <div id="nav" style="margin:0;padding:0;">
       <el-container>
-        <el-header>
+        <el-header style="padding:0">
           <el-menu
+            :default-active="activeIndex"
+            @select="handleSelect"
             mode="horizontal"
             background-color="#545c64"
             text-color="#fff"
             active-text-color="#ffd04b"
+            style="padding-left: 45px"
           >
-            <el-menu-item index="1">
-              <router-link to="/">文件转换</router-link>
-            </el-menu-item>
-            <el-menu-item index="2">
-              <router-link to="/about">模板管理</router-link>
-            </el-menu-item>
+            <el-menu-item index="1">文件转换</el-menu-item>
+            <el-menu-item index="2">模板管理</el-menu-item>
           </el-menu>
         </el-header>
         <el-main>
@@ -27,7 +26,24 @@
 
 <script lang="ts">
 import Vue from "vue";
+import router from "vue-router";
 export default Vue.extend({
+  data() {
+    return {
+      activeIndex: "1",
+      path: {
+        "1": { path: "/" },
+        "2": { path: "/about" }
+      }
+    };
+  },
+  methods: {
+    handleSelect(key: string): void {
+      let dest: any = this.path[key];
+      console.log(dest);
+      this.$router.push(dest);
+    }
+  }
 });
 </script>
 
