4fa8b3ff2f7d7cd9df5f1c18998d5853d29631dc
shiqiang <shiqiang@jd.com>
2020-01-06 17:42:03 +0800

    删除error代码

--- src/core/service/Service.ts
+++ src/core/service/Service.ts
@@ -25,7 +25,6 @@ ipcMain.on('convertDoc', (event, data: { index: number, data: Doc }) => {
 });
 
 ipcMain.on('queryTemplate', (event, param: { pageNum: number, size: number }) => {
-    throw Error('errorLo!');
     let pageNum = 1;
     if (param.pageNum > 1) {
         pageNum = param.pageNum;
